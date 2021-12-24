fe6e497fd85041460cc55068ee69601fd35762b5
Wosley Vacemberg <wosleyv@gmail.com>
2021-12-24 01:56:12 -0300

    add NEXT_PUBLIC_SOLANA_NETWORK

--- components/WalletConnectionProvider.tsx
+++ components/WalletConnectionProvider.tsx
@@ -10,7 +10,8 @@ import { FC, useMemo } from "react";
 
 const WalletConnectionProvider: FC = ({ children }) => {
   // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = "devnet" as WalletAdapterNetwork;
+  const network = process.env
+    .NEXT_PUBLIC_SOLANA_NETWORK as WalletAdapterNetwork;
 
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
